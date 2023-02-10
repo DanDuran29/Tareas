489f3a4230fb8787abb6f5c8969f4d35ca5dd4b0
Danilo Duran <23004523@galileo.edu.gt>
2023-02-10 11:54:51 -0600

    actividad 3

---
+++ CalculadoraEstaciones.js
@@ -0,0 +1,60 @@
+var mes = parseInt (prompt("ingrese el numero de un mes"));
+
+
+if(mes == 12){
+    
+    alert("es invierno");
+
+} else if(mes == 1){
+
+    alert("es invierno");
+
+} else if(mes == 2){
+
+    alert("es invierno");
+
+} else if(mes == 3){
+  
+    alert("es primavera");  
+
+} else if(mes == 4){
+
+    alert("es primavera");
+
+} else if(mes == 5){
+
+    alert("es primavera");
+
+} else if(mes == 6){
+
+    alert("es verano");
+
+} else if(mes == 7){
+
+
+    alert("es verano");
+
+} else if(mes == 8){
+
+    alert("es verano");
+
+}else if(mes == 9){
+
+alert("es otoño");
+
+} else if(mes == 10){
+
+    alert("es otoño");
+
+} else if(mes== 11){
+
+    alert("es otoño");
+
+} else {
+
+    alert("valor invalido");
+}
+
+alert("Danilo Durán 23004523");
+
+
